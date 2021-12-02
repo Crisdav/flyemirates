cb6ec4b6e0841b7aa53071418dbecb039b9246e2
crisdav <c.d.m.o11once@gmail.com>
2021-12-01 22:55:25 -0500

    listar-eliminar todos

---
+++ src/app/modelos/vuelo.model.ts
@@ -0,0 +1,10 @@
+export class VueloModelo{
+    id?: String;
+    fecha_inicio?: String;
+    hora_inicio?: String;
+    fecha_fin?: String;
+    hora_fin?: String;
+    asientos_vendidos?: String;
+    nombre_piloto?: String;
+    ruta?: String;
+    }
